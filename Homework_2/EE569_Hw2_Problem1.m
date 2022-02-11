4c709ea1c3951c70fc0f0b995618ff04a2f39d80
Boyang Xiao <sean_xiaoby@163.com>
2022-02-11 11:05:09 -0800

    Add ignore file and README

--- Homework_2/EE569_Hw2_Problem1.m
+++ Homework_2/EE569_Hw2_Problem1.m
@@ -206,11 +206,35 @@ writeraw(CannyResult2*255, extractBefore(FileName2, ".raw")+"_CannyEdge.raw");
 
 %% Problem 1 - C
 
-% clear;
+clear;
 
+addpath(genpath('./'));
+addpath(genpath('edges-master'));
+addpath(genpath('toolbox-master'));
 
+% % load pre-trained edge detection model and set opts (see edgesDemo.m)
+% model=load('models/forest/modelBsds'); model=model.model;
 
+opts=edgesTrain();                % default options (good settings)
+opts.modelDir='models/';          % model will be in models/forest
+opts.modelFnm='modelBsds';        % model name
+opts.nPos=5e5; opts.nNeg=5e5;     % decrease to speedup training
+opts.useParfor=0;                 % parallelize if sufficient memory
 
+% train edge detector (~20m/8Gb per tree, proportional to nPos/nNeg)
+model=edgesTrain(opts); % will load model if already trained
+
+% set detection parameters (can set after training)
+model.opts.multiscale=0;          % for top accuracy set multiscale=1
+model.opts.sharpen=2;             % for top speed set sharpen=0
+model.opts.nTreesEval=4;          % for top speed set nTreesEval=1
+model.opts.nThreads=4;            % max number threads for evaluation
+model.opts.nms=0;                 % set to true to enable nms
+
+% detect edge and visualize results
+I = imread('Tiger.jpg');
+tic, E=edgesDetect(I,model); toc
+figure(1); im(I); figure(2); im(1-E);
 
 
 
