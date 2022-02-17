7a5a7bab6942033f8b3785823e8e0f06176f20e7
SeanXiaoby <sean_xiaoby@163.com>
2022-02-17 12:04:00 -0800

    Hw2 dithering finished.

--- Homework_2/EE569_Hw2_Problem2.m
+++ Homework_2/EE569_Hw2_Problem2.m
@@ -23,7 +23,7 @@ try
     sgtitle("Problem 2-a: Different Half-toning results");
 catch
 end
-subplot(2,2,1);
+subplot(2,3,1);
 imshow(OriginalData1,[]);
 title("original Image");
 
@@ -36,7 +36,7 @@ for nRow = 1: Row
         if OriginalData1(nRow, nCol) >= Threshold1  HTresults1(nRow, nCol) = 255;   end
     end
 end
-subplot(2,2,2);
+subplot(2,3,2);
 imshow(HTresults1,[]);
 title("Half-tone with fixed threshold");
 writeraw(HTresults1, "bridge_Halftone_fixed.raw");
@@ -52,20 +52,68 @@ for nRow = 1: Row
         if OriginalData1(nRow, nCol) >= Treshold2  HTresults2(nRow, nCol) = 255;   end
     end
 end
-subplot(2,2,3);
+subplot(2,3,3);
 imshow(HTresults2,[]);
 title("Half-tone with random threshold");
 writeraw(HTresults2, "bridge_Halftone_random.raw");
 
-% Dithering
+%% Dithering
+
+% Firstly apply I2 dithering
+DTresults1 = zeros(Row, Col);
+IMkernal1 = [1,2;3,0];
+IMthreshold1 = (IMkernal1 + 0.5) * 255 / power(2,2);
+
+for nRow = 1: Row
+    for nCol = 1:Col
+        Treshold = IMthreshold1(mod(nRow, 2)+1, mod(nCol, 2)+1);
+        if OriginalData1(nRow, nCol) >= Treshold  DTresults1(nRow, nCol) = 255;   end
+    end
+end
+
+subplot(2,3,4);
+imshow(DTresults1,[]);
+title("Half-tone with DIthering I2");
+writeraw(DTresults1, "bridge_Halftone_dithering2.raw");
+
+%Apply I8 dithering
+IMkernal2 = [IMkernal1*4+1 IMkernal1*4+2; IMkernal1*4+3 IMkernal1*4];
+IMkernal3 = [IMkernal2*4+1 IMkernal2*4+2; IMkernal2*4+3 IMkernal2*4];
+
+DTresults2 = zeros(Row, Col);
+IMthreshold3 = (IMkernal3 + 0.5) * 255 / power(8,2);
+
+for nRow = 1: Row
+    for nCol = 1:Col
+        Treshold = IMthreshold3(mod(nRow, 8)+1, mod(nCol, 8)+1);
+        if OriginalData1(nRow, nCol) >= Treshold  DTresults2(nRow, nCol) = 255;   end
+    end
+end
+
+subplot(2,3,5);
+imshow(DTresults2,[]);
+title("Half-tone with DIthering I8");
+writeraw(DTresults2, "bridge_Halftone_dithering8.raw");
 
 
+%Apply I32 dithering
+IMkernal4 = [IMkernal3*4+1 IMkernal3*4+2; IMkernal3*4+3 IMkernal3*4];
+IMkernal5 = [IMkernal4*4+1 IMkernal4*4+2; IMkernal4*4+3 IMkernal4*4];
 
+DTresults3 = zeros(Row, Col);
+IMthreshold5 = (IMkernal5 + 0.5) * 255 / power(32,2);
 
+for nRow = 1: Row
+    for nCol = 1:Col
+        Treshold = IMthreshold5(mod(nRow, 32)+1, mod(nCol, 32)+1);
+        if OriginalData1(nRow, nCol) >= Treshold  DTresults3(nRow, nCol) = 255;   end
+    end
+end
 
-
-
-
+subplot(2,3,6);
+imshow(DTresults2,[]);
+title("Half-tone with DIthering I32");
+writeraw(DTresults2, "bridge_Halftone_dithering32.raw");
 
 
 
