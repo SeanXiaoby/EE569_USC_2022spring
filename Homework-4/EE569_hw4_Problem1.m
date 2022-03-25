a274ffaa0d3ec07490366ec7c31ecf54456e4b3d
SeanXiaoby <sean_xiaoby@163.com>
2022-03-24 22:55:53 -0700

    Hw4 updated

--- Homework-4/EE569_hw4_Problem1.m
+++ Homework-4/EE569_hw4_Problem1.m
@@ -67,13 +67,39 @@ for i = 1:4
             end
             EnergyFeature(fi) = mean(mean(FilterResponse));
         end
-        EnergyFeatures(i, j) = EnergyFeature;
+        EnergyFeatures{i, j} = EnergyFeature;
     end
 end
 
+PCAdata = zeros(36,25);
+
+for i = 1:4
+    for j = 1:9
+        PCAdata(i*9-9+j, : ) = EnergyFeatures{i,j}(1,:);
+    end
+end
+
+PCAdata = PCAdata(:, 2:25);
+
+PCAres = pca(PCAdata,"NumComponents",3);
+PCAres = PCAdata*PCAres;
+
+figure;
+hold on
+scatter3(PCAres(1:9,1),PCAres(1:9,2),PCAres(1:9,3),"blue");
+scatter3(PCAres(10:18,1),PCAres(10:18,2),PCAres(10:18,3),"black");
+scatter3(PCAres(19:27,1),PCAres(19:27,2),PCAres(19:27,3),"green");
+scatter3(PCAres(28:36,1),PCAres(28:36,2),PCAres(28:36,3),"red");
+legend("Blacket", "Brick", "Grass", "Stones")
+title("Training samples features visualization")
+grid on
+hold off
 
 
+%% Part(b)
 
+idx = kmeans(PCAdata,4);
+idx2 = kmeans(PCAres,4);
 
 
 
